0a3845236f9871e78a6fe08e87988c1e7ec77804
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-01-22 21:50:47 +0530

    Add files via upload

---
+++ portfolio/src/components/WorkCardData.js
@@ -0,0 +1,51 @@
+import pro2 from '../assets/project1.png'
+import pro1 from '../assets/project2.png'
+import pro3 from '../assets/project3.png'
+import pro4 from '../assets/project4.png'
+import pro5 from '../assets/project5.png'
+
+const ProjectCardData = [
+
+  {
+    imgsrc: pro1,
+    title: 'Netflix-Responsive Clone Frontend using Reactjs',
+    text: "This application uses TMDB API for fetching list of movies as per users' preference. Axios is used for API request. Post completion, the application has been successully deployed and hosted online using Firebase.",
+    view: "https://netflix-clone-5cf5c.web.app/",
+    source: "https://github.com/st908503/React_projects/tree/main/netflix-responsive"
+  },
+
+  {
+    imgsrc: pro2,
+    title: 'Tesla Clone Frontend using Reactjs',
+    text: "Tesla Clone is a responsive homepage of Tesla website built using reactJs. The application makes use of react-icons and react-router-dom. Reusable components have been designed to break the code for simplicity.",
+    view: "https://teslawebsiteclone.netlify.app/",
+    source: "https://github.com/st908503/React_projects/tree/main/tesla"
+  },
+
+ 
+
+  {
+    imgsrc: pro3,
+    title: 'Tinder Clone Frontend using Reactjs',
+    text: "Tinder Clone is a basic frontend landing page for Tinder website where client can swipe the users based on their interests. It uses Material UI for adding beautiful icons such as swipe-left, swipe-right, superlike etc.  ",
+    view: "https://tinderwebsiteclone.netlify.app/",
+    source: "https://github.com/st908503/React_projects/tree/main/tinder-clone-frontend"
+  },
+  {
+    imgsrc: pro4,
+    title: 'React Calculator',
+    text: "React calculator is a basic calculator which provides the user to perform basic mathematical operations like addition, subtraction, multiplication etc. The application can be viewed on both desktop and mobile screens ",
+    view: "https://calculator-b7591.web.app/",
+    source: "https://github.com/st908503/React_projects/tree/main/React_calc"
+  },
+
+  {
+    imgsrc: pro5,
+    title: 'React Todo List',
+    text: "This is a basic todo-list built using react where a user can add, delete the items. The application also allows user to delete all items in a single click. The application can be viewed on both desktop and mobile screens",
+    view: "https://todo-list-b3720.web.app/",
+    source: "https://github.com/st908503/React_projects/tree/main/todo-list"
+  },
+]
+
+export default ProjectCardData;
